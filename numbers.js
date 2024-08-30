ee181374fdf9a77c31e98d2f59c6f511c0461718
Igor Małyszko <igor.malyszko33@gmail.com>
2024-08-30 12:43:57 +0200

    init
    praca domowa

---
+++ numbers.js
@@ -0,0 +1,9 @@
+const numbers = [2, 8, 3, 17, 1, 385, 4];
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    
+    if (number > 5) {
+        console.log(number);
+    }
+}
